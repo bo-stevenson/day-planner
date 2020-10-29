c962345098992c81eafa3494ad0cc0821e7020c9
Bo Stevenson <bostevenson1027@gmail.com>
2020-10-29 18:23:49 -0500

    added current day

--- script.js
+++ script.js
@@ -2,6 +2,45 @@
 $(document).ready(function(){
 
 
+let today = moment().format("MMMM Do YYYY");
+console.log(today);
+$("#currentDay").append(today);
+
+
+let currentTime = parseInt(moment().format("HH"));
+console.log(currentTime);
+
+let textNine = $("9AM");
+let textTen = $("10AM");
+let textEleven = $("11AM");
+let textTwelve = $("12PM");
+let textOne = $("1PM");
+let textTwo = $("2PM");
+let textThree = $("3PM");
+let textFour = $("4PM");
+let textFive = $("5PM");
+
+
+$("textarea").each(function(){
+
+ let hour = parseInt($(this).attr("name"));
+    if (hour < currentTime){
+        $(this).addClass("past");
+    } 
+
+    if (hour === currentTime){
+        $(this).addClass("present");
+    } 
+
+    if (hour > currentTime){
+        $(this).addClass("future");
+    } 
+
+
+
+
+
+});
 
 
 
